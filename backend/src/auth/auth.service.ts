50822a2ab600a46e750a245b0ea3ffb57ff5818f
amorev <shikto@mail.ru>
2022-03-27 11:58:23 +0300

    #3 Signup mechanics refactoring
    * extract access token creation

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -6,78 +6,76 @@ import { User } from '../users/users.entity';
 
 @Injectable()
 export class AuthService {
-  private readonly logger = new Logger(AuthService.name);
+    private readonly logger = new Logger(AuthService.name);
 
-  constructor(
-    public usersService: UsersService,
-    private jwtService: JwtService,
-  ) {}
-
-  async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
-    if (user && (await this.usersService.validateUser(user, pass))) {
-      const { ...result } = user;
-      return result;
+    constructor(
+        public usersService: UsersService,
+        private jwtService: JwtService,
+    ) {
     }
-    return null;
-  }
 
-  expireTimeAccess = 60 * 60 * 24 * 3;
-  expireTimeRefresh = 60 * 60 * 24 * 14;
-
-  makeRefreshToken(user: User) {
-    const payload = { username: user.username, sub: user.id };
-    return this.jwtService.sign(payload, {
-      secret: jwtConstants.refreshSecret,
-      expiresIn: this.expireTimeRefresh,
-    });
-  }
-
-  async login(user: User) {
-    try {
-      const payload = { username: user.username, sub: user.id };
-      const refreshToken = this.makeRefreshToken(user);
-      await this.usersService.setCurrentRefreshToken(refreshToken, user.id);
-      return {
-        access_token: this.jwtService.sign(payload),
-        refresh_token: refreshToken,
-        expireTime: this.expireTimeAccess,
-      };
-    } catch (e) {
-      this.logger.error('Auth error: ' + e);
-      throw new Error("Auth error");
+    async validateUser(username: string, pass: string): Promise<any> {
+        const user = await this.usersService.findOne(username);
+        if (user && (await this.usersService.validateUser(user, pass))) {
+            const {...result} = user;
+            return result;
+        }
+        return null;
     }
-  }
 
-  async signup(userInfo: {
-    username: string,
-    password: string
-  }) {
-    try {
-      const [user] = await this.usersService.createUser(userInfo.username,userInfo.password);
-      const payload = { username: user.username, sub: user.id };
-      const refreshToken = this.makeRefreshToken(user);
-      await this.usersService.setCurrentRefreshToken(refreshToken, user.id);
-      return {
-        access_token: this.jwtService.sign(payload),
-        refresh_token: refreshToken,
-        expireTime: this.expireTimeAccess,
-      };
-    } catch (e) {
-      this.logger.error('Auth error: ' + e);
-      throw new Error("Signup error");
+    expireTimeAccess = 60 * 60 * 24 * 3;
+    expireTimeRefresh = 60 * 60 * 24 * 14;
+
+    makeRefreshToken(user: User) {
+        const payload = {username: user.username, sub: user.id};
+        return this.jwtService.sign(payload, {
+            secret: jwtConstants.refreshSecret,
+            expiresIn: this.expireTimeRefresh,
+        });
     }
-  }
 
-  async refreshToken(user: User) {
-    const payload = { username: user.username, sub: user.id };
+    async login(user: User) {
+        try {
+            return this.createAccessToken(user);
+        } catch (e) {
+            this.logger.error('Auth error: ' + e);
+            throw new Error("Auth error");
+        }
+    }
 
-    const refreshToken = await this.makeRefreshToken(user);
-    await this.usersService.setCurrentRefreshToken(refreshToken, user.id);
-    return {
-      access_token: this.jwtService.sign(payload),
-      refresh_token: refreshToken,
-      expireTime: this.expireTimeRefresh,
-    };
-  }
+    async signup(userInfo: {
+        username: string,
+        password: string
+    }) {
+        try {
+            const [user] = await this.usersService.createUser(userInfo.username, userInfo.password);
+            return this.createAccessToken(user);
+        } catch (e) {
+            this.logger.error('Auth error: ' + e);
+            throw new Error("Signup error");
+        }
+    }
+
+    private async createAccessToken(user: { password: string; username: string } & User) {
+        const payload = {username: user.username, sub: user.id};
+        const refreshToken = this.makeRefreshToken(user);
+        await this.usersService.setCurrentRefreshToken(refreshToken, user.id);
+        return {
+            access_token: this.jwtService.sign(payload),
+            refresh_token: refreshToken,
+            expireTime: this.expireTimeAccess,
+        };
+    }
+
+    async refreshToken(user: User) {
+        const payload = {username: user.username, sub: user.id};
+
+        const refreshToken = await this.makeRefreshToken(user);
+        await this.usersService.setCurrentRefreshToken(refreshToken, user.id);
+        return {
+            access_token: this.jwtService.sign(payload),
+            refresh_token: refreshToken,
+            expireTime: this.expireTimeRefresh,
+        };
+    }
 }
